66d1e7e73346cabc499d2e75da5b9d390c2b713f
bednep222 <sebastianbeltrangaliano@gmail.com>
2026-09-16 21:49:44 -0500

    Version 1

---
+++ README.md
@@ -0,0 +1,15 @@
+# Registro de datos
+
+## Datos del Proyecto
+- Lenguaje elegido: Python
+- Curso: Fundamento de programacion
+
+## Estudiante
+-  Sebastian Lizandro Beltrán Galiano - N00522237
+
+
+## Descripción y Requerimientos
+Es la "Guía de Laboratorio Tema 2", profe si lee esto perdon pero tuve q usa ia y un video de youtube para crear el repositorio y poder guardar mis versiones del trabajo por q no sabia como hacerlo.
+
+## Referencias
+- Video Tutorial: [Implementación y Automátas](https://www.youtube.com/watch?v=zN84-aabOSk)
--- registros.py
+++ registros.py
@@ -1,20 +1,7 @@
 
-def Validar_codigo(Codigo):
-    Longitud_codigo = 8
-    if Codigo == "" or len(Codigo) > Longitud_codigo:
-        print("El codigo no puede estar vacio o tener mas de 8 caracteres")
-        return False
-    else:
-        return True    
-
-
-
 def Mostrar_datos():
     Nombre = str(input("Ingrese su nombre y apellido: "))
     codigo = str(input("Ingrese su código: "))
-    while not Validar_codigo(codigo):
-        print("Ingrese su codigo nuevamente")
-        codigo = str(input("Ingrese su código: "))
     Consulta = str(input("Ingrese el tipo de consulta:"))
     Descripcion = str(input("Desciba de forma breve su consulta:"))
     return Nombre, codigo, Consulta, Descripcion
