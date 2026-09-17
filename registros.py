7d0a9f9fcf3988e8774da304e0a9af7e1b9a67a2
bednep222 <sebastianbeltrangaliano@gmail.com>
2026-09-16 20:37:01 -0500

    Version 1.1

--- registros.py
+++ registros.py
@@ -4,6 +4,8 @@ def Mostrar_datos():
     Nombre = str(input("Ingrese su nombre: "))
     Codigo = str(input("Ingrese su código: "))
     Consulta = str(input("Ingrese el tipo de consulta:"))
-    return Nombre, Codigo, Consulta
+    Descripcion = str(input("Desciba de forma breve su consulta:"))
+    return Nombre, Codigo, Consulta, Descripcion
 
 Mostrar_datos()
+
