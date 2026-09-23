42f767f62c1aff131e784e6990cb7798e7141437
bednep222 <sebastianbeltrangaliano@gmail.com>
2026-09-23 18:18:22 -0500

    Version 4

--- registros.py
+++ registros.py
@@ -1,31 +1,38 @@
+def Validar_nombre(Nombre):
+  Nombre = Nombre.strip()
+  if Nombre == "":
+    print("Ingrese un nombre valido e intentalo nuevamente")
+    return False
+  else:
+    return True
+
 def Validar_codigo(Codigo):
   Longitud_codigo = 8
-  if Codigo == "" or len(Codigo) > Longitud_codigo:
-    print("El codigo no puede estar vacio o tener mas de 8 caracteres")
+  if len(Codigo) != Longitud_codigo:
+    print("El codigo no puede estar vacio o tener mas de 8 caracteres, ingrese su codigo nuevamente")
     return False
   else:
    return True 
 
-  
 def Validar_tipoconsulta(Consulta):
  tipos_consulta = ["matricula", "pagos", "constancias", "certificaciones", "otros"]
  if Consulta not in tipos_consulta:
-  print("El tipo de consulta no es válido")  
+  print("El tipo de consulta no es válido, ingrese un tipo de consulta válido")
   return False
  else:
   return True
 
 
 def Mostrar_datos():
-    Nombre = str(input("Ingrese su nombre y apellido: "))
+    Nombre = str(input("Ingrese su nombre: "))
+    while not Validar_nombre(Nombre):
+      Nombre = str(input("Ingrese su nombre: "))
     codigo = str(input("Ingrese su código: "))
     while not Validar_codigo(codigo):
-        print("Ingrese su codigo nuevamente")
-        codigo = str(input("Ingrese su código: "))
+      codigo = str(input("Ingrese su código: "))
     Consulta = str(input("Ingrese el tipo de consulta:"))
     while not Validar_tipoconsulta(Consulta):
-        print("Igrese un tipo de consulta valido")
-        Consulta = str(input("Ingrese el tipo de consulta: "))
+      Consulta = str(input("Ingrese el tipo de consulta: "))
     Descripcion = str(input("Desciba de forma breve su consulta:"))
     return Nombre, codigo, Consulta, Descripcion
 
