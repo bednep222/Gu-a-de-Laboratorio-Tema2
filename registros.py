bc203d221ed526d812562225049387a5a1103318
bednep222 <sebastianbeltrangaliano@gmail.com>
2026-09-23 23:37:02 -0500

    Version 5

--- registros.py
+++ registros.py
@@ -8,6 +8,7 @@ def Validar_nombre(Nombre):
 
 def Validar_codigo(Codigo):
   Longitud_codigo = 8
+  Codigo = Codigo.strip()
   if len(Codigo) != Longitud_codigo:
     print("El codigo no puede estar vacio y debe tener 8 caracteres, ingrese su codigo nuevamente")
     return False
@@ -15,7 +16,7 @@ def Validar_codigo(Codigo):
    return True 
 
 def Validar_tipoconsulta(Consulta):
- consulta = consulta.strip().lower()
+ Consulta = Consulta.strip().lower()
  tipos_consulta = ["matricula", "pagos", "constancias", "certificaciones", "otros"]
  if Consulta not in tipos_consulta:
   print("El tipo de consulta no es válido, ingrese un tipo de consulta válido")
@@ -24,34 +25,49 @@ def Validar_tipoconsulta(Consulta):
   return True
 
 def Prioridad(Consulta):
-    Texto = "Alta","Media","Baja"
+    Consulta = Consulta.strip().lower()
     if Consulta == "matricula":
-     return "Alta"
+     return "Prioridad Alta"
     elif Consulta == "pagos":
-       return "Alta"
+       return "Prioridad Alta"
     elif Consulta == "constancias":
-     return "Media"
+     return "Prioridad Media"
     elif Consulta == "certificaciones":
-     return "Media"
+     return "Prioridad Media"
     elif Consulta == "otros":
-      return "Baja"
+      return "Prioridad Baja"
     else:
       return "Error de prioridad"
+
+def Resumen_datos(Nombre, codigo, Consulta, Descripcion, Nivel):
+  print("-Solicitud-")
+  print("Nombre: ", Nombre)
+  print("Codigo: ", codigo)
+  print("Consulta: ", Consulta)
+  print("Descripcion: ", Descripcion)
+  print("Nivel:", Nivel)
+
     
 def Mostrar_datos():
     Nombre = str(input("Ingrese su nombre: "))
     while not Validar_nombre(Nombre):
       Nombre = str(input("Ingrese su nombre: "))
+
     codigo = str(input("Ingrese su código: "))
+    codigo = codigo.strip()
     while not Validar_codigo(codigo):
       codigo = str(input("Ingrese su código: "))
+      codigo = codigo.strip()
+
     Consulta = str(input("Ingrese el tipo de consulta:"))
+    Consulta = Consulta.strip().lower()
     while not Validar_tipoconsulta(Consulta):
       Consulta = str(input("Ingrese el tipo de consulta: "))
-      Prioridad(Consulta)
-      print (nivel)
-  
-    Descripcion = str(input("Desciba de forma breve su consulta:"))
-    return Nombre, codigo, Consulta, Descripcion, Prioridad
+      Consulta = Consulta.strip().lower()
+    nivel = Prioridad(Consulta)
+    print(nivel)
+    Descripcion = str(input("Describa de forma breve su consulta: "))
+    return Nombre, codigo, Consulta, Descripcion, nivel
 
-Mostrar_datos()
+Nombre, codigo, Consulta, Descripcion, nivel = Mostrar_datos()
+Mostrar_datos(Nombre, codigo, Consulta, Descripcion, nivel)
